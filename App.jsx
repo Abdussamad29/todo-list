195be703e640e5732dc99f014f6340dd6576e564
Abdussamad khan <69312871+Abdussamad29@users.noreply.github.com>
2024-03-15 12:13:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Todo from './components/todo.jsx'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Todo/>
+     
+    </>
+  )
+}
+
+export default App
